3df149858aff691755255af28bb54eb92ffc83a4
Alessio Giay <s310703@studenti.polito.it>
2025-03-12 23:37:29 +0100

    Primo upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+double map(double x)
+{
+	return(((x-1.0)*(3.0)/4.0)-1.0);
+}
+int main()
+{
+	int count = 0;
+	double Mean = 0.0;
+    std::ifstream input("data.txt");
+    std::ofstream output("result.txt");
+    
+    if (!input) 
+    {
+        std::cerr << "Errore nell'apertura del file di input" << std::endl;
+        return 1;
+    }
+    if (!output) 
+    {
+        std::cerr << "Errore nell'apertura del file di output" << std::endl;
+        return 1;
+    }
+    output<<"#\tN Mean\n";
+
+    std::string line;
+    double n;
+	while (input >> n)
+	{
+        count++;
+        double N = map(n);
+        Mean = (Mean * (count-1) + N)/(count);
+        output<<count<<"\t"<<std::setprecision(16)<<std::scientific<<Mean<<std::endl;
+    }
+
+    return 0;
+}
