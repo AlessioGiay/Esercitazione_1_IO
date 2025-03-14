9ccd7cd9de66de823e7eaca7705b13846c4515b0
Alessio Giay <s310703@studenti.polito.it>
2025-03-14 10:41:31 +0100

    Aggiunta dei file per la funzione e aggiornamento CMake e main

---
+++ function.cpp
@@ -0,0 +1,6 @@
+#include "function.h"
+
+double map(double x)
+{
+	return(((x-1.0)*(3.0)/4.0)-1.0);
+}
--- main.cpp
+++ main.cpp
@@ -2,11 +2,8 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include "function.h"
 
-double map(double x)
-{
-	return(((x-1.0)*(3.0)/4.0)-1.0);
-}
 int main()
 {
 	int count = 0;
